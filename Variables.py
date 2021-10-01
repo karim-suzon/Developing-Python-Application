8bfa25f260a01a8c88a90e8d0f9df18c0d7812c9
karim-suzon <ak.suzon@hotmail.com>
2021-10-01 18:55:43 +0300

    Question 1

---
+++ Variables.py
@@ -0,0 +1,18 @@
+a = 999999
+b = 5.555555555
+c = 'x'
+d = "kokkola"
+e = 2.33
+f = 10
+g = 300
+#h = 9 billions # in python we can not define the variables like this
+# i = 3 billions # this wrong way to declare the variables
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+# print(type(h)) # it will print invalid syntax
+# print(type(i)) # it will print invalid syntax
