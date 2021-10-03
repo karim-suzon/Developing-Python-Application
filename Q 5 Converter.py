f22df3c8642545e6008b0725fbbc7c510e6cf7a9
karim-suzon <ak.suzon@hotmail.com>
2021-10-03 12:41:59 +0300

    new solution

--- Q 5 Converter.py
+++ Q 5 Converter.py
@@ -1,1 +1,11 @@
-converter
+from forex_python.converter import CurrencyRates
+import datetime as dt
+
+date = dt.datetime(2022, 10, 3)
+c = CurrencyRates()
+
+#countries = ["EUR", "USD"]
+
+print('Currency as of {}'.format(date))
+print(c.convert('USD','EUR',1),"eur")
+
