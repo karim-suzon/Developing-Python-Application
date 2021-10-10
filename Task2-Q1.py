61cefab21dbb255bb0c2c84c376958f1d1efc005
karim-suzon <ak.suzon@hotmail.com>
2021-10-10 12:43:54 +0300

    new commit

--- Task2-Q1.py
+++ Task2-Q1.py
@@ -1,11 +1,30 @@
+# Take three values from user and find the greatest number from them
+
+#a, b, c= int(input('enter first number')), int(input('enter second number')), int(input('enter third number'))
+#nums= [a, b, c]
+#print(max(nums))
+
+#x,y,z = int(input("1st num")),int(input("2nd num")),int(input("3rd num"))
+#if x>y and x>z :
+    #print(x)
+#elif y>z:
+    #print(y)
+#else :
+    #print(z)
+
+
+
+
 #User gives a value and our program tells if the value is > 100 or not
 
-user = int(input("pls enter a value : "))
+user_number = int(input("pls enter a value : "))
 
 
-if user > 100:
-    print("value is greater than 100,", 'input number is : ',user)
-elif user == 100:
-    print("value is equal",user)
+if user_number > 100:
+    print("value is greater than 100,", 'input number is : ',user_number)
+elif user_number == 100:
+    print("value is equal",user_number)
 else:
-    print("the value is less than 100,", 'input number is : ',user)
+    print("the value is less than 100,", 'input number is : ',user_number)
+
+
---
+++ Task3-Q2.py
@@ -0,0 +1,35 @@
+# this is not difficult,this just unfamilair . you just need practice it.the more you practice this concept,it will make your concept very clear.
+
+#User enters a weekday number and the program tells the name of the day
+
+#viikko = ["Lauantai","Sunnuntai","Maanantai","Tiistai","Keskiviikko","Torstai","Perjantai"]
+
+
+# Taken day number from user
+
+
+viikko = int(input("Enter viikko päivä numero (1-7) : "))
+
+if viikko == 1:
+    print("\nMaanantai")
+
+elif viikko == 2:
+    print("\nTistai")
+
+elif (viikko == 3):
+    print("\nKeskivikko")
+
+elif (viikko == 4):
+    print("\nTorstai")
+
+elif (viikko == 5):
+    print("\nPerjantai")
+
+elif (viikko == 6):
+    print("\nLauantai")
+
+elif (viikko == 7):
+    print("\nSunnuntai")
+
+else:
+    print("\nPlease enter viikko numero between 1-7.")
