9af0e75128e139969a8e9498323089734fe48672
karim-suzon <ak.suzon@hotmail.com>
2021-10-11 20:31:14 +0300

    new

--- Task3-Q5.py
+++ Task3-Q5.py
@@ -1,1 +1,18 @@
-#User gives the lengths of the triangle's sides.Program tells what is the triangle like(e.g. is it right angled,isosceles...)
+#User gives the lengths of the triangle's sides.
+# Program tells what is the triangle like
+
+#An equilateral triangle is a triangle in which all three sides are equal.
+#A scalene triangle is a triangle that has three unequal sides.
+#An isosceles triangle is a triangle with (at least) two equal sides.
+
+print("Input lengths of the triangle sides: ")
+x = int(input("x: "))
+y = int(input("y: "))
+z = int(input("z: "))
+
+if x == y == z:
+	print("Equilateral triangle")
+elif x==y or y==z or z==x:
+	print("isosceles triangle")
+else:
+	print("Scalene triangle")
