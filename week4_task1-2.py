7e822091ab729fbcb73e7dc93777a4db495a6f8e
karim-suzon <ak.suzon@hotmail.com>
2021-10-18 19:28:34 +0300

    week4

---
+++ week4_task1-2.py
@@ -0,0 +1,10 @@
+#Program calculates the sum of values 1-5.
+#Use: for and while
+
+sum = 0
+
+j = 1                             #initialize
+while j < 5:                      #condition
+    sum = sum + j
+    j = j+ 1                      #increment
+print(sum)
---
+++ week4_task1.py
@@ -0,0 +1,8 @@
+#Program calculates the sum of values 1-5.
+#Use: for and while
+
+
+sum = 0
+for i in range(1, 5):
+    sum += i          # sum = sum + i
+print(sum)
