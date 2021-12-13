70086591a26d78ed15b21ed4f56c8f3ad5369525
karim-suzon <ak.suzon@hotmail.com>
2021-12-13 23:12:32 +0200

    week10

---
+++ week10_Python_Module.py
@@ -0,0 +1,27 @@
+
+
+import math
+
+print(math.ceil(3.36)) # it will print 4
+print(math.ceil(-4.4)) # it will print -4
+
+
+print(math.floor(4.5)) # it will print 4
+print(math.floor(-3.35)) # it will print -4
+
+import time
+
+
+print(time.time())
+print(time.gmtime())
+print(time.localtime())
+print(time.ctime())
+
+
+from datetime import *
+
+print(datetime.now())
+
+
+
+
---
+++ week10_Testing & Debugging.py
@@ -0,0 +1,12 @@
+
+
+
+def sum_of_n(n):
+    sum = 0
+    for k in range(1,n+1,1):
+        sum += k
+    return sum
+
+# call
+print(sum_of_n(5))
+
